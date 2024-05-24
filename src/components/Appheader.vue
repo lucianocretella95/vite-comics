f392781c4b47731141ecc8d01b6aa95beb39cbe3
lucianocretella95 <luciano.cretella@tim.it>
2024-05-24 16:03:00 +0200

    components

--- src/components/Appheader.vue
+++ src/components/Appheader.vue
@@ -1,14 +1,13 @@
 <script>
 export default {
     data() {
-        message: "titolo"
     }
 }
 </script>
 <template>
     <h1>testo di prova</h1>
 </template>
-<style scoped>
+<style>
 h1 {
     background-color: aquamarine;
     color: black;
